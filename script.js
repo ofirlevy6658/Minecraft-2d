50634b9eb87aaa837b78250157ccff64a79dd9af
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-02-24 23:38:52 +0200

    add grass

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const world = document.querySelector(".world");
 let grid = [];
-const groundHeight = 29; //the top height of ground use to build trees and stuff
+const groundHeight = 35; //the top height of ground use to build trees and stuff
 const groundLength = 40;
 //
 //init the world grid consists of divs
 const initMatrix = () => {
-	for (let i = 0; i < 30; i++) {
+	for (let i = 0; i < 40; i++) {
 		grid[i] = [];
 		for (let j = 0; j < groundLength; j++) {
 			const div = document.createElement("div");
@@ -19,10 +19,11 @@ const initMatrix = () => {
 const initGround = () => {
 	for (let i = groundHeight; i < 40; i++) {
 		for (let j = 0; j < groundLength; j++) {
-			const div = document.createElement("div");
-			div.classList.add("square");
-			div.classList.add("ground");
-			world.appendChild(div);
+			if (i === groundHeight)
+				//first iteration for grass
+				grid[i][j].classList.add("grass");
+			else grid[i][j].classList.add("ground");
+			world.appendChild(grid[i][j]);
 		}
 	}
 };
@@ -30,13 +31,14 @@ const initGround = () => {
 const drawTree = (numOfTree) => {
 	while (numOfTree > 0) {
 		let random = Math.floor(Math.random() * (groundLength - 3)) + 2; // random place on the ground to build tree
-		let heigtTree = Math.floor(Math.random() * 15) + 1;
+		let heightTree = Math.floor(Math.random() * 15) + 3;
+		console.log(heightTree);
 		//stump
-		for (let i = 0; i < heigtTree; i++) {
-			grid[groundHeight - i][random].classList.add("stump");
+		for (let i = 0; i < heightTree; i++) {
+			grid[groundHeight - i - 1][random].classList.add("stump");
 		}
 		//leaves
-		for (let i = heigtTree; i < heigtTree + 6; i++) {
+		for (let i = heightTree; i < heightTree + 6; i++) {
 			grid[groundHeight - i][random].classList.add(`leaves${random % 2}`); //create two version of leaves
 			grid[groundHeight - i][random - 1].classList.add(`leaves${random % 2}`);
 			grid[groundHeight - i][random + 1].classList.add(`leaves${random % 2}`);
